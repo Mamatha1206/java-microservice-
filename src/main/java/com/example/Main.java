dc7c0f509a288ea7d108197c451e760f399d2c99
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-04-10 14:32:40 +0530

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        // Printing a simple message to the console
         System.out.println("Hello, World!");
     }
 }
+
