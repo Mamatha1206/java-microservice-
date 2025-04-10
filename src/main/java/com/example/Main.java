eadacb0c998630dc783af47f65f9467c7fbc4155
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-04-10 14:08:52 +0530

    Create Main.java

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Printing a simple message to the console
+        System.out.println("Hello, World!");
+    }
+}
