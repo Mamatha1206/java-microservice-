6dc427ff00705b4514473357458e1358161b14a2
Mamatha Eudulakanti <119730722+Mamatha1206@users.noreply.github.com>
2025-04-10 14:05:51 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,8 +1,6 @@
-// src/main/java/com/example/HelloWorld.java
-
 package com.example;
 
-public class HelloWorld {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
